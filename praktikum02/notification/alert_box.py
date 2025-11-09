b81157d9643c687787373979076635e031785e27
faizabdullah0134 <faizahf0134@gmail.com>
2025-11-10 01:24:14 +0700

    Praktikum 2

---
+++ praktikum02/notification/alert_box.py
@@ -0,0 +1,15 @@
+import streamlit as st
+
+st.title("Praktikum 2 - Alert Box Notifications")
+st.write("Kelompok 2:")
+st.markdown("""
+- Faiz Abdullah Hanif Firmansyah - 0110222281
+- Jamilatun Khoerunnisa - 0110222254
+- Alim Rifai - 0110122068
+""")
+
+st.success("Successful")
+st.warning("Warning")
+st.info("Information")
+st.error("Error")
+st.exception("It is an exception")
